c59978c5781c95ae3661036f16f5d30e592494cd
Vanessa Freudenberg <vanessa@codefrau.net>
2024-07-11 19:45:58 -0700

    Directly use local version, not from jsdelivr

--- website/vite.config.ts
+++ website/vite.config.ts
@@ -2,15 +2,16 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import ViteYaml from '@modyfi/vite-plugin-yaml'
 import tsconfigPaths from 'vite-tsconfig-paths'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 import { version as CROQUET_VERSION } from './node_modules/@croquet/croquet/package.json'
 
 // During development, we want to load Croquet via <SCRIPT> tag
 // instead of bundling it with the rest of the code
 // so that we can use source maps and debug the Croquet code.
-// (even better would be if we could load Croquet as an external verbatim chunk,
-// but that surpasses my Vite knowledge for now --codefrau)
+// To make this work, we need to fake an ES module because Vite
+// needs that but the Croquet package does not have an ES build yet.
 
-const CROQUET_URL = `https://cdn.jsdelivr.net/npm/@croquet/croquet@${CROQUET_VERSION}`;
+const CROQUET_FILE = `croquet-${CROQUET_VERSION}.min.js`;
 const CROQUET_MODULE_SRC = `
   // fake ES module exports
   const { Model, View, Session, App, Constants, Data } = window.Croquet;
@@ -18,23 +19,25 @@ const CROQUET_MODULE_SRC = `
 `;
 
 // Vite plugin to inject and use Croquet via <SCRIPT> tag
-function CroquetViaScriptTag(mode: string) {
-  const name = 'croquet-via-script-tag';
-  if (mode !== 'development') return { name }; // no-op in production
+function CroquetViaScriptTag() {
   return {
-    name,
+    name: 'croquet-via-script-tag',
     transformIndexHtml: {
       order: "pre",
       handler() {
-        console.log(`Inserting Croquet ${CROQUET_VERSION} <SCRIPT> tag`)
         return [
-          { tag: 'script', attrs: { src: CROQUET_URL } },
+          {
+            injectTo: 'head',
+            tag: 'script',
+            attrs: {
+              src: `assets/${CROQUET_FILE}`
+            }
+          },
         ];
       }
     } as any, // variant with order spec not in the type definition yet
     load(filepath) {
       if (filepath.includes("@croquet/croquet")) {
-        console.log(`Using Croquet ${CROQUET_VERSION} from <SCRIPT> tag`)
         return CROQUET_MODULE_SRC
       }
       return null
@@ -42,12 +45,21 @@ function CroquetViaScriptTag(mode: string) {
   }
 }
 
-export default defineConfig(( { mode } ) => ({
+export default defineConfig({
   optimizeDeps: {
     exclude: ["@croquet/croquet"], // enable transforms even in dev mode
   },
   plugins: [
-    CroquetViaScriptTag(mode),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'node_modules/@croquet/croquet/pub/croquet.min.js',
+          dest: 'assets',
+          rename: CROQUET_FILE
+        },
+      ],
+    }),
+    CroquetViaScriptTag(),
     ViteYaml(),
     react(),
     tsconfigPaths()
@@ -68,4 +80,4 @@ export default defineConfig(( { mode } ) => ({
       },
     },
   },
-}))
+})
