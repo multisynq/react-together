c4eb2323b900c569b932256d0c26d9d2d6a378cd
elijah lee <84287663+aloha-elijah@users.noreply.github.com>
2024-07-14 22:29:58 -0700

    Update FeatureBanner.tsx

--- website/src/components/sections/FeatureBanner.tsx
+++ website/src/components/sections/FeatureBanner.tsx
@@ -1,16 +1,16 @@
 export function FeatureBanner() {
   return (
-    <div className='flex flex-col md:flex-row w-full gap-10 py-8'>
-      <div className='flex-1 min-w-0'>
+    <div className='flex flex-col md:flex-row w-full gap-10 py-8 items-center'>
+      <div className='flex-1'>
         <h3 className='mb-4'>Feature Title</h3>
         <p>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
           enim ad minim veniam, quis nostrud exercitation ullamco laboris.
         </p>
       </div>
-      <div className='flex-1 min-w-0'>
-        <div className='w-full aspect-[5/3] bg-gray-200'></div>
+      <div className='flex-1'>
+        <div className='w-full aspect-[5/3] bg-gray-100 rounded-lg'></div>
       </div>
     </div>
-  );
+  )
 }
