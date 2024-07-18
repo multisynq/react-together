26784d534339204f7f4a7c1c214c1de0773a9a5b
elijah lee <84287663+aloha-elijah@users.noreply.github.com>
2024-07-18 10:06:17 -0700

    Update GifDisplay.tsx
    updated css to be on center

--- website/src/components/ui/GifDisplay.tsx
+++ website/src/components/ui/GifDisplay.tsx
@@ -1,7 +1,7 @@
 export function GifDisplay({ imageSource, imageTitle }) {
   return (
     <>
-      <div className='border-[1.5px] border-black rounded-lg w-full'>
+      <div className='border-[1.5px] border-black rounded-lg w-full flex justify-center items-center'>
         <img src={imageSource} alt={imageTitle} className='max-w-[20rem]' />
       </div>
     </>
