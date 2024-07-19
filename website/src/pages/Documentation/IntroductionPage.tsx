69090dc426f51e9b09eade05c9b671823bafe778
elijah lee <84287663+aloha-elijah@users.noreply.github.com>
2024-07-19 11:55:32 -0700

    Update IntroductionPage.tsx
    From google docs update on 'July 19, 10:32 AM'

--- website/src/pages/Documentation/IntroductionPage.tsx
+++ website/src/pages/Documentation/IntroductionPage.tsx
@@ -19,49 +19,38 @@ export const IntroductionPage: React.FC = () => {
   return (
     <>
       <h3 id='introduction'>Introduction</h3>
+      <p>In the future, web sites are all live and connect visitors with each other all the time. That future is now!</p>
+      <p>
+        Welcome to React Together! With this library you will be able to build synchronized web applications in a blink of an eye! No need
+        to set up servers or sockets!
+      </p>
+      {/* <p>
+        {`Now, at a fairly top level of your React's JSX, you will need to:\n1. Wrap everything in a ReactTogether tag so you can connect a (free) API key.`}
+      </p> */}
+      {/* <CodeBlock language='javascript' code1={sampleCode2} /> */}
+      <h3 id='installation'>Installation</h3>
       <p>In your shiny React project, you will need to install the react-together package.</p>
       <CodeBlock language='javascript' code1={sampleCode1} />
       <p>
-        {`Now, at a fairly top level of your React's JSX, you will need to:\n1. Wrap everything in a ReactTogether tag so you can connect a (free) API key.`}
-      </p>
-      <CodeBlock language='javascript' code1={sampleCode2} />
-      <p>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
         ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
         reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt
         in culpa qui officia deserunt mollit anim id est laborum.
       </p>
-      <p>
+      {/* <h3 id='download'>Download</h3> */}
+      {/* <CodeBlockExample /> */}
+      {/* <p>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
         ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
         reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt
         in culpa qui officia deserunt mollit anim id est laborum.
-      </p>
-      <h3 id='installation'>Installation</h3>
-      <CodeBlockExample />
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-        ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-        reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt
-        in culpa qui officia deserunt mollit anim id est laborum.
-      </p>
-      <h3 id='download'>Download</h3>
-      <CodeBlockExample />
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-        ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-        reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt
-        in culpa qui officia deserunt mollit anim id est laborum.
-      </p>
-      <DocumentDemoBox />
-      <CodeBlockExample />
+      </p> */}
+      {/* <DocumentDemoBox /> */}
+      {/* <CodeBlockExample /> */}
       <h3 id='context'>Context</h3>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-        ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-        reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt
-        in culpa qui officia deserunt mollit anim id est laborum.
-      </p>
+      <p>Now, at a fairly top level of your React’s JSX, you will need to:</p>
+      <p>Wrap everything in a ReactTogether tag so you can connect a (free) API key.</p>
+      <CodeBlock language='javascript' code1={sampleCode2} />
     </>
   )
 }
@@ -69,6 +58,6 @@ export const IntroductionPage: React.FC = () => {
 export const introductionNavItems: NavItem[] = [
   { key: 'introduction', label: 'Introduction' },
   { key: 'installation', label: 'Installation' },
-  { key: 'download', label: 'Download' },
+  // { key: 'download', label: 'Download' },
   { key: 'context', label: 'Context' },
 ]
