5f289e5cdad70df398a07861b2846b1260249108
elijah lee <84287663+aloha-elijah@users.noreply.github.com>
2024-07-19 17:32:03 -0700

    Update DocumentationPage.tsx
    w-full without needing the hero demo

--- website/src/pages/DocumentationPage.tsx
+++ website/src/pages/DocumentationPage.tsx
@@ -94,7 +94,8 @@ export function DocumentationPage() {
   const { content, navItems } = Component()
 
   return (
-    <main className='flex justify-center items-start gap-12 self-stretch p-6 px-16 relative'>
+    // <main className='flex justify-center items-start gap-12 self-stretch p-6 px-16 relative'>
+    <main className='flex justify-center items-start gap-12 p-6 px-16 relative bg-[radial-gradient(162.17%_100%_at_100%_0%,#f3f7fd_0%,#f3f7fd_23%,#FFF_100%)]'>
       <div className='hidden sm:block h-full'>
         <DocumentNav />
       </div>
