1661b4465919e2de74ed4cbd67b87ebd8c7bcf1c
afonsocrg <afonso.crg@gmail.com>
2024-07-22 19:44:53 +0100

    remove together flag from examples

--- src/main.tsx
+++ src/main.tsx
@@ -6,22 +6,17 @@ import { ReactTogether } from '../react-together/index.ts'
 import App from './App.tsx'
 import './index.css'
 
-const together = true
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <PrimeReactProvider>
-      {together ? (
-        <ReactTogether
-          sessionParams={{
-            appId: import.meta.env['VITE_APP_ID'],
-            apiKey: import.meta.env['VITE_API_KEY']
-          }}
-        >
-          <App />
-        </ReactTogether>
-      ) : (
+      <ReactTogether
+        sessionParams={{
+          appId: import.meta.env['VITE_APP_ID'],
+          apiKey: import.meta.env['VITE_API_KEY']
+        }}
+      >
         <App />
-      )}
+      </ReactTogether>
     </PrimeReactProvider>
   </React.StrictMode>
 )
