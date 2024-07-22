c3c393a09b3dc39814fce02a6aee6c1cd0d9c5c8
elijah lee <84287663+aloha-elijah@users.noreply.github.com>
2024-07-21 17:18:49 -0700

    FeatureCard ui update
    Feature card from 3 by 1 to row ui to individual ui

--- website/src/components/ui/FeatureCard.tsx
+++ website/src/components/ui/FeatureCard.tsx
@@ -1,48 +1,52 @@
-// function GifContainer() {
-//   return <div className='bg-gray-200 aspect-video w-full '></div>
-// }
-
-// interface TextContainerProps {
-//   header: string
-//   body: string
-// }
-
-// function TextContainer({ header, body }: TextContainerProps) {
-//   return (
-//     <div className='w-full flex flex-col items-center px-12 my-4 gap-2'>
-//       <h3 className='text-center'>{header}</h3>
-//       <p className='text-center'>{body}</p>
-//     </div>
-//   )
-// }
-
-function EachFeature({ header, body }) {
+function EachFeature({ header, body, color, imgSource }) {
   return (
-    <div className='w-full'>
-      <div className='bg-gray-200 aspect-video w-full '></div>
-      <div className='w-full flex flex-col items-center px-12 my-4 gap-2'>
-        <h3 className='text-center'>{header}</h3>
-        <p className='text-center'>{body}</p>
+    <div className={`${color} w-full border-[2.5px] border-gray-800 shadow-lineStyle rounded-xl overflow-hidden`}>
+      <div className={'aspect-video w-full shadow-md'}>
+        <img src={imgSource} />
+      </div>
+      <div className='w-full flex flex-col items-center px-4 my-8'>
+        <h4 className='text-center font-bold'>{header}</h4>
+        <p className='text-center text-gray-800 mb-1'>{body}</p>
       </div>
     </div>
   )
 }
 
 export function FeatureCard() {
-  const title1 = '1. Install'
-  const title2 = '2. Wrap'
-  const title3 = '3. Enhance!'
+  const title1 = 'Install'
+  const title2 = 'Wrap'
+  const title3 = 'Enhance!'
 
-  const gif1 = ''
-  const gif2 = ''
-  const gif3 = ''
+  const color1 = 'bg-indigo-200'
+  const color2 = 'bg-rose-200'
+  const color3 = 'bg-green-200'
+
+  const gif1 = 'src/images/npm_install.gif'
+  const gif2 = '../../images/npm_install.gif'
+  const gif3 = '../../images/npm_install.gif'
 
   return (
     <span className='w-full px-8'>
-      <div className='w-full flex flex-col md:flex-row border-2 border-gray-700 shadow-lineStyle rounded-lg overflow-hidden'>
-        <EachFeature header={title1} body={'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.'} />
-        <EachFeature header={title2} body={'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum'} />
-        <EachFeature header={title3} body={'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia '} />
+      {/* <div className='w-full flex flex-col md:flex-row border-2 border-gray-500 shadow-lineStyleSecond rounded-xl overflow-hidden'> */}
+      <div className='w-full flex flex-col md:flex-row gap-6'>
+        <EachFeature
+          header={title1}
+          body={'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.'}
+          color={color1}
+          imgSource={gif1}
+        />
+        <EachFeature
+          header={title2}
+          body={'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum'}
+          color={color2}
+          imgSource={gif1}
+        />
+        <EachFeature
+          header={title3}
+          body={'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia '}
+          color={color3}
+          imgSource={gif1}
+        />
       </div>
     </span>
   )
--- website/tailwind.config.js
+++ website/tailwind.config.js
@@ -51,6 +51,7 @@ module.exports = {
         },
       },
       borderRadius: {
+        xl: 'calc(var(--radius) * 2)',
         lg: 'var(--radius)',
         md: 'calc(var(--radius) - 2px)',
         sm: 'calc(var(--radius) - 4px)',
