b9e8e6c6b7b08142ab18bc9e6d06dca25c6e8dec
elijah lee <84287663+aloha-elijah@users.noreply.github.com>
2024-07-23 09:36:15 -0700

    Headers type change

--- website/src/App.tsx
+++ website/src/App.tsx
@@ -21,6 +21,7 @@ export default function App() {
         <link rel='preconnect' href='https://fonts.googleapis.com' />
         <link href='https://fonts.googleapis.com/css?family=Comfortaa' rel='stylesheet'></link>
         <link href='https://fonts.googleapis.com/css?family=Inter' rel='stylesheet'></link>
+        <link href='https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700&display=swap' rel='stylesheet' />
       </Helmet>
       <MainContext.Provider value={{}}>
         <BrowserRouter>
