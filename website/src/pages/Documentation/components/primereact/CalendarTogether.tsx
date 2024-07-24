6a152f1911bc54b791c7c75e24be09d1d3ee29a6
elijah lee <84287663+aloha-elijah@users.noreply.github.com>
2024-07-24 16:22:51 -0700

    Update CalendarTogether.tsx
    updated pre google doc

--- website/src/pages/Documentation/components/primereact/CalendarTogether.tsx
+++ website/src/pages/Documentation/components/primereact/CalendarTogether.tsx
@@ -1,6 +1,9 @@
+import { TableContainer } from '@components/ui/TableContainer'
 import { GenericDocNav } from '@pages/documentation/GenericDocPage'
 import { DocumentationSkeleton } from '@pages/DocumentationSkeleton'
 // import { CalendarTogether } from '../../../../../../../react-together'
+import NewPropSpan from '@components/ui/NewPropSpan'
+import RemovedPropSpan from '@components/ui/RemovedPropSpan'
 import { PrimeReactComponentDocumentationPage } from './PrimeReactComponentDocumentationPage'
 
 function PrimeReactCalendarTogetherDemo() {
@@ -13,12 +16,37 @@ function PrimeReactCalendarTogetherDemo() {
 }
 
 export default function PrimeReactCalendarTogetherDocumentationPage() {
+  const api = (
+    <>
+      <h5>Params</h5>
+      <TableContainer
+        keys={[
+          { key: 'name', label: 'Name' },
+          { key: 'type', label: 'Type' },
+          { key: 'default', label: 'Default Value' },
+          { key: 'description', label: 'Description' },
+        ]}
+        data={[
+          {
+            name: <NewPropSpan text='rtid' />,
+            type: 'string',
+            description: 'The key used to identify this state, passed to the useStateTogether hook',
+          },
+          {
+            name: <RemovedPropSpan text='value' />,
+            description: 'Removed, since this value will be controlled by the useStateTogether hook',
+          },
+        ]}
+      />
+    </>
+  )
   const content = (
     <PrimeReactComponentDocumentationPage
       name='CalendarTogether'
       originalName='Calendar'
       docUrl={`https://primereact.org/calendartogether/`}
       ComponentDemo={PrimeReactCalendarTogetherDemo}
+      api={api}
     />
   )
 
