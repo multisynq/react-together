c7b192b52804f1a1ede1bb1b72cf2561fb957e3f
elijah lee <84287663+aloha-elijah@users.noreply.github.com>
2024-07-24 15:51:36 -0700

    Create OverfillSpan.tsx
    For table description that is overfilled

---
+++ website/src/components/ui/OverfillSpan.tsx
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+
+interface OverfillSpanProps {
+  text?: string
+}
+
+const TRUNCATION_LENGTH = 54
+
+export default function OverfillSpan({ text = '' }: OverfillSpanProps) {
+  const [isExpanded, setIsExpanded] = useState(false)
+
+  const toggleText = () => {
+    setIsExpanded(!isExpanded)
+  }
+
+  const displayedText = isExpanded ? text : text.substring(0, TRUNCATION_LENGTH) + (text.length > TRUNCATION_LENGTH ? '...' : '')
+
+  return (
+    <div>
+      <span>{displayedText}</span>
+      {text.length > TRUNCATION_LENGTH && (
+        <button className='rounded-md px-2 border-1 border-gray-700 shadow-lineStyle py-0 ml-2' onClick={toggleText}>
+          {isExpanded ? '-' : '+'}
+        </button>
+      )}
+    </div>
+  )
+}
