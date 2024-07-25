921617f79fb1fdf2f10459691db0610f5935e43c
afonsocrg <afonso.crg@gmail.com>
2024-07-25 13:37:48 +0100

    add note explaining why we are omitting readOnly prop form PrimeReact Rating component

--- react-together/components/primereact/RatingTogether.tsx
+++ react-together/components/primereact/RatingTogether.tsx
@@ -2,6 +2,9 @@ import { Rating, RatingProps } from 'primereact/rating'
 import { Nullable } from 'primereact/ts-helpers'
 import { useStateTogether } from '../../hooks'
 
+// There are some implications about making this component readOnly
+// For example: How to ensure every user sees the same readOnly state
+// for this component?
 export interface RatingTogetherProps
   extends Omit<RatingProps, 'value' | 'onChange' | 'readOnly'> {
   rtid: string
