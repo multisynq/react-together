774cb6143099fbfb39c7c3c5287241b5fbd54253
afonsocrg <afonso.crg@gmail.com>
2024-07-25 21:10:51 +0100

    remove commented unused line

--- website/src/pages/Documentation/DocumentationPage.tsx
+++ website/src/pages/Documentation/DocumentationPage.tsx
@@ -6,7 +6,6 @@ interface DocumentationPageProps {
 }
 export function DocumentationPage({ content, navItems }: DocumentationPageProps) {
   return (
-    // <main className='flex justify-center items-start gap-12 self-stretch p-6 px-16 relative'>
     <>
       <div className='flex flex-col items-start gap-[1.5rem] w-full'>{content}</div>
       <PageNav items={navItems} />
