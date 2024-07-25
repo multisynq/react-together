fcd3dad14a842e7024cb88350232699546c80bd6
afonsocrg <afonso.crg@gmail.com>
2024-07-25 20:24:23 +0100

    use router  instead of changing window location, to avoid unnecessary reloads

--- website/src/components/ui/DocumentNav.tsx
+++ website/src/components/ui/DocumentNav.tsx
@@ -1,41 +1,50 @@
-import { MenuItem } from 'primereact/menuitem'
+import { MenuItem, MenuItemCommandEvent } from 'primereact/menuitem'
 import { PanelMenu } from 'primereact/panelmenu'
+import { useNavigate } from 'react-router-dom'
 // import { useNavigate } from 'react-router-dom'
 
-// Add `key` property to MenuItem and to its nested elements
+// Add `key` and `to` properties to MenuItem and to its nested elements
 export interface PatchedMenuItem extends MenuItem {
   key?: string
   items?: PatchedMenuItem[]
+  to?: string
+  command?: (e: PatchedMenuItemCommandEvent) => void
+}
+interface PatchedMenuItemCommandEvent extends MenuItemCommandEvent {
+  item: PatchedMenuItem
 }
 
 const items: PatchedMenuItem[] = [
   {
     label: 'Get Started',
     expanded: true,
-    items: [{ label: 'Introduction' }, { label: 'Configuration' }],
+    items: [
+      { label: 'Introduction', to: '/introduction' },
+      { label: 'Installation', to: '/installation' },
+    ],
   },
   {
     label: 'Components',
     expanded: true,
     items: [
-      { label: 'ReactTogether', url: '/ReactTogether' },
-      { label: 'ConnectedViews', url: '/ConnectedViews' },
-      { label: 'PresenceDiv', url: '/PresenceDiv' },
+      { label: 'ReactTogether', to: '/ReactTogether' },
+      { label: 'ConnectedViews', to: '/ConnectedViews' },
+      { label: 'PresenceDiv', to: '/PresenceDiv' },
       {
         label: 'Prime React',
         expanded: true,
         items: [
-          { label: 'CalendarTogether', url: '/primereact/Calendar' },
-          { label: 'CheckboxTogether', url: '/primereact/Checkbox' },
-          { label: 'DropdownTogether', url: '/primereact/Dropdown' },
-          { label: 'InputSwitchTogether', url: '/primereact/InputSwitch' },
-          { label: 'KnobTogether', url: '/primereact/Knob' },
-          { label: 'MultiSelectTogether', url: '/primereact/MultiSelect' },
-          { label: 'RatingTogether', url: '/primereact/Rating' },
-          { label: 'SelectButtonTogether', url: '/primereact/SelectButton' },
-          { label: 'TabViewTogether', url: '/primereact/TabView' },
-          { label: 'ToggleButtonTogether', url: '/primereact/ToggleButton' },
-          { label: 'TriStateCheckboxTogether', url: '/primereact/TriStateCheckbox' },
+          { label: 'CalendarTogether', to: '/primereact/Calendar' },
+          { label: 'CheckboxTogether', to: '/primereact/Checkbox' },
+          { label: 'DropdownTogether', to: '/primereact/Dropdown' },
+          { label: 'InputSwitchTogether', to: '/primereact/InputSwitch' },
+          { label: 'KnobTogether', to: '/primereact/Knob' },
+          { label: 'MultiSelectTogether', to: '/primereact/MultiSelect' },
+          { label: 'RatingTogether', to: '/primereact/Rating' },
+          { label: 'SelectButtonTogether', to: '/primereact/SelectButton' },
+          { label: 'TabViewTogether', to: '/primereact/TabView' },
+          { label: 'ToggleButtonTogether', to: '/primereact/ToggleButton' },
+          { label: 'TriStateCheckboxTogether', to: '/primereact/TriStateCheckbox' },
         ],
       },
     ],
@@ -44,20 +53,20 @@ const items: PatchedMenuItem[] = [
     label: 'Hooks',
     expanded: true,
     items: [
-      { label: 'useStateTogether', url: '/useStateTogether' },
-      { label: 'useStateTogetherWithPerUserValues', url: '/useStateTogetherWithPerUserValues' },
-      { label: 'useConnectedViews', url: '/useConnectedViews' },
-      { label: 'useHoveringViews', url: '/useHoveringViews' },
-      { label: 'useIsTogether', url: '/useIsTogether' },
-      { label: 'useConnectNewSession', url: '/useConnectNewSession' },
-      { label: 'useLeaveSession', url: '/useLeaveSession' },
+      { label: 'useStateTogether', to: '/useStateTogether' },
+      { label: 'useStateTogetherWithPerUserValues', to: '/useStateTogetherWithPerUserValues' },
+      { label: 'useConnectedViews', to: '/useConnectedViews' },
+      { label: 'useHoveringViews', to: '/useHoveringViews' },
+      { label: 'useIsTogether', to: '/useIsTogether' },
+      { label: 'useConnectNewSession', to: '/useConnectNewSession' },
+      { label: 'useLeaveSession', to: '/useLeaveSession' },
     ],
   },
   {
     // label: 'Discover',
     // expanded: true,
     // items: [
-    //   { label: 'About Us', url: 'https://multisynq.io/' },
+    //   { label: 'About Us', to: 'https://multisynq.io/' },
     //   { label: 'Roadmap' },
     //   { label: 'Support' },
     //   { label: 'Contributing' },
@@ -67,21 +76,30 @@ const items: PatchedMenuItem[] = [
   },
 ]
 
-function processMenuItems(items: PatchedMenuItem[], prefix: string | null = null) {
-  let expandedKeys = {}
-  items.forEach((i, idx) => {
-    const key = prefix ? `${prefix}.${idx}` : `${idx}`
-    i.key = key
-    if (i.expanded && i.items) {
-      expandedKeys[i.key] = true
-      expandedKeys = { ...expandedKeys, ...processMenuItems(i.items, key) }
-    }
-  })
-  return expandedKeys
-}
-const expandedKeys = processMenuItems(items)
-
 export default function DocumentNav() {
+  const navigate = useNavigate()
+
+  // We have to process the items inside the component body because
+  // we need to use the navigate hook.
+  // This has room to be improved
+  function processMenuItems(items: PatchedMenuItem[], prefix: string | null = null) {
+    let expandedKeys = {}
+    items.forEach((i, idx) => {
+      const key = prefix ? `${prefix}.${idx}` : `${idx}`
+      i.command = ({ item }) => {
+        if (item.to) {
+          navigate(item.to)
+        }
+      }
+
+      if (i.expanded && i.items) {
+        expandedKeys[i.key] = true
+        expandedKeys = { ...expandedKeys, ...processMenuItems(i.items, key) }
+      }
+    })
+    return expandedKeys
+  }
+  const expandedKeys = processMenuItems(items)
   return (
     <PanelMenu
       model={items}
