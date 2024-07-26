48ac91b90c759d5b9a56d81a100b687569607674
Vanessa Freudenberg <vanessa@codefrau.net>
2024-07-26 12:02:42 -0700

    Actually fix cloudflare build

--- cloudflare-build.sh
+++ cloudflare-build.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-# cloudflare installs arm versions from package-lock.json
+# cloudflare clean-installs arm versions from package-lock.json
 # but actuallty runs on x64 so we need to install the x64 versions
-npm i || exit 1
+npm i \
+    @ast-grep/napi-linux-x64-gnu \
+    @rollup/rollup-linux-x64-gnu \
+    @swc/core-linux-x64-gnu \
+|| exit 1
 
 # build the react-together package
 npm run build || exit 1
