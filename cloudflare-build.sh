4641d31972f8c797f8c8942f717e800facb12927
Vanessa Freudenberg <vanessa@codefrau.net>
2024-07-26 12:33:21 -0700

    Update cloudflare-build.sh

--- cloudflare-build.sh
+++ cloudflare-build.sh
@@ -1,4 +1,19 @@
-#!/bin/sh
+#!/bin/bash
+
+# cloudflare clean-installs arm versions from package-lock.json
+# but actuallty runs on x64 so we need to install the x64 versions
+if [ -n "$CF_PAGES" ]; then
+    echo "Running on Cloudflare Pages, installing x64 versions"
+    npm i \
+        @ast-grep/napi-linux-x64-gnu \
+        @rollup/rollup-linux-x64-gnu \
+        @swc/core-linux-x64-gnu \
+    || exit 1
+else
+    echo "Running on local machine, deleting node_modules and clean-installing packages"
+    rm -rf node_modules website/node_modules
+    npm ci || exit 1
+fi
 
 # build the react-together package
 npm run build
