7ecaa6856e2974d2324deea4764b74b333613f7b
afonsocrg <afonso.crg@gmail.com>
2024-07-30 13:02:58 +0100

    lint vite.croquet.ts

--- website/vite.croquet.ts
+++ website/vite.croquet.ts
@@ -8,8 +8,8 @@
 // Set the CROQUET_SOURCEMAPS environment variable to the source maps
 // directory URL, otherwise you have to manually add the source map
 
-import fs from 'fs';
-import { defineConfig as viteDefineConfig, Plugin, ConfigEnv, UserConfig, UserConfigFn, UserConfigExport } from 'vite'
+import fs from 'fs'
+import { ConfigEnv, Plugin, UserConfig, UserConfigExport, UserConfigFn, defineConfig as viteDefineConfig } from 'vite'
 import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 // We want to load Croquet via <SCRIPT> tag
@@ -20,45 +20,45 @@ import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 // find the Croquet package in node_modules
 // it can be either in @croquet/croquet or a dependency of @croquet/react
-let CROQUET_PKG = 'node_modules/@croquet/croquet';
+let CROQUET_PKG = 'node_modules/@croquet/croquet'
 if (!fs.existsSync(CROQUET_PKG)) {
-  CROQUET_PKG = 'node_modules/@croquet/react/' + CROQUET_PKG;
+  CROQUET_PKG = 'node_modules/@croquet/react/' + CROQUET_PKG
   if (!fs.existsSync(CROQUET_PKG)) {
-    throw new Error('Cannot find @croquet/croquet package in node_modules');
+    throw new Error('Cannot find @croquet/croquet package in node_modules')
   }
 }
 // get the version of the Croquet package
-const CROQUET_VERSION = JSON.parse(fs.readFileSync(`${CROQUET_PKG}/package.json`, 'utf8')).version;
+const CROQUET_VERSION = JSON.parse(fs.readFileSync(`${CROQUET_PKG}/package.json`, 'utf8')).version
 // grab script source path from package
-const CROQUET_SCRIPT_PATH = `${CROQUET_PKG}/pub/croquet.min.js`;
+const CROQUET_SCRIPT_PATH = `${CROQUET_PKG}/pub/croquet.min.js`
 // target name for the script file
-const CROQUET_SCRIPT_NAME = `croquet-${CROQUET_VERSION}.min.js`;
+const CROQUET_SCRIPT_NAME = `croquet-${CROQUET_VERSION}.min.js`
 // fake ES module to make Vite happy
 const CROQUET_MODULE_SRC = `
   const { Model, View, Session, App, Constants, Data } = window.Croquet;
   export { Model, View, Session, App, Constants, Data };
-`;
+`
 
 // Vite plugin to inject and use Croquet via <SCRIPT> tag
-function CroquetViaScriptTag():Plugin {
+function CroquetViaScriptTag(): Plugin {
   return {
     name: 'croquet-via-script-tag',
     transformIndexHtml: {
-      order: "pre",
+      order: 'pre',
       handler() {
         return [
           {
             injectTo: 'head',
             tag: 'script',
             attrs: {
-              src: `assets/${CROQUET_SCRIPT_NAME}`
-            }
+              src: `assets/${CROQUET_SCRIPT_NAME}`,
+            },
           },
-        ];
-      }
+        ]
+      },
     },
     load(filepath) {
-      if (filepath.includes("@croquet/croquet")) {
+      if (filepath.includes('@croquet/croquet')) {
         return CROQUET_MODULE_SRC
       }
       return null
@@ -68,23 +68,21 @@ function CroquetViaScriptTag():Plugin {
 
 // Append source map URL to the Croquet library
 const CroquetAddSourcemap = (mode) => {
-  const sourcemaps = process.env.CROQUET_SOURCEMAPS;
-  if (!sourcemaps || mode !== 'development') return undefined;
+  const sourcemaps = process.env.CROQUET_SOURCEMAPS
+  if (!sourcemaps || mode !== 'development') return undefined
   return (contents) => {
-    const marker = '\n//# sourceMappingURL';
+    const marker = '\n//# sourceMappingURL'
     if (!contents.includes(marker)) {
-      contents += `${marker}=${sourcemaps}/${CROQUET_SCRIPT_NAME}.map\n`;
+      contents += `${marker}=${sourcemaps}/${CROQUET_SCRIPT_NAME}.map\n`
     }
-    return contents;
+    return contents
   }
 }
 
 // Croquet config for Vite
-const croquetConfigFn = ({mode}): UserConfig => ({
+const croquetConfigFn = ({ mode }): UserConfig => ({
   optimizeDeps: {
-    exclude: [
-      "@croquet/croquet"
-    ],
+    exclude: ['@croquet/croquet'],
   },
   plugins: [
     viteStaticCopy({
@@ -99,40 +97,40 @@ const croquetConfigFn = ({mode}): UserConfig => ({
     }),
     CroquetViaScriptTag(),
   ],
-});
+})
 
 // merge the Croquet config with the user config
 function mergeCroquetConfig(userConfig: UserConfig, env: ConfigEnv): UserConfig {
-  const croquetConfig = croquetConfigFn(env);
+  const croquetConfig = croquetConfigFn(env)
   // add to plugins
-  if (userConfig.plugins) userConfig.plugins.push(...croquetConfig.plugins);
-  else userConfig.plugins = croquetConfig.plugins;
+  if (userConfig.plugins) userConfig.plugins.push(...croquetConfig.plugins)
+  else userConfig.plugins = croquetConfig.plugins
   // add to optimizeDeps.exclude
   if (userConfig.optimizeDeps) {
-    if (userConfig.optimizeDeps.exclude) userConfig.optimizeDeps.exclude.push(...croquetConfig.optimizeDeps.exclude);
-    else userConfig.optimizeDeps.exclude = croquetConfig.optimizeDeps.exclude;
+    if (userConfig.optimizeDeps.exclude) userConfig.optimizeDeps.exclude.push(...croquetConfig.optimizeDeps.exclude)
+    else userConfig.optimizeDeps.exclude = croquetConfig.optimizeDeps.exclude
   } else {
-    userConfig.optimizeDeps = croquetConfig.optimizeDeps;
+    userConfig.optimizeDeps = croquetConfig.optimizeDeps
   }
-  return userConfig;
+  return userConfig
 }
 
 // we are mimicking the Vite defineConfig function
 // to allow all of the Croquet configuration to be merged
 export async function defineConfig(userConfigExport: UserConfigExport): Promise<UserConfigFn> {
   // could be a promise, if so, resolve it
-  let userConfigOrFn = await userConfigExport;
+  const userConfigOrFn = await userConfigExport
   // could be a function or object
   if (typeof userConfigOrFn === 'function') {
     // it's a function, return a new function that merges the Croquet config
-    const userConfigFn = userConfigOrFn;
+    const userConfigFn = userConfigOrFn
     return async (env: ConfigEnv) => {
-      const userConfig = await userConfigFn(env);
-      return viteDefineConfig(mergeCroquetConfig(userConfig, env));
+      const userConfig = await userConfigFn(env)
+      return viteDefineConfig(mergeCroquetConfig(userConfig, env))
     }
   } else {
     // it's an object, return a function that merges the Croquet config
-    const userConfig = userConfigOrFn;
-    return (env) => viteDefineConfig(mergeCroquetConfig(userConfig, env));
+    const userConfig = userConfigOrFn
+    return (env) => viteDefineConfig(mergeCroquetConfig(userConfig, env))
   }
 }
