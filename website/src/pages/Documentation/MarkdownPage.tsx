8eedd5602a42dd4d380b74aba94e8a02301c8410
elijah lee <84287663+aloha-elijah@users.noreply.github.com>
2024-08-02 14:18:23 -0700

    Update MarkdownPage.tsx
    Styling update

--- website/src/pages/Documentation/MarkdownPage.tsx
+++ website/src/pages/Documentation/MarkdownPage.tsx
@@ -8,30 +8,32 @@ interface MarkdownPageProps {
 }
 export function MarkdownPage({ markdown }: MarkdownPageProps) {
   return (
-    <div className='max-w-5xl mx-36'>
-      <Markdown
-        remarkPlugins={[remarkGfm]}
-        rehypePlugins={[rehypeRaw]}
-        className='markdown-content'
-        components={{
-          // a: ({ node, ...props }) => {
-          //   return <Link to={node.properties.href as string} target='_blank' {...props} />
-          // },
-          code: ({ className, children, ...props }) => {
-            const isInline = children.toString().split('\n').length === 1
-            const match = /language-(\w+)/.exec(className || '')
-            return !isInline ? (
-              <CodeBlock language={match?.[1]} code1={String(children)} {...props} />
-            ) : (
-              <code className={className} {...props}>
-                {children}
-              </code>
-            )
-          },
-        }}
-      >
-        {markdown}
-      </Markdown>
+    <div className='w-full'>
+      <div className='max-w-[1324px] line-border bg-white px-5'>
+        <Markdown
+          remarkPlugins={[remarkGfm]}
+          rehypePlugins={[rehypeRaw]}
+          className='markdown-content'
+          components={{
+            // a: ({ node, ...props }) => {
+            //   return <Link to={node.properties.href as string} target='_blank' {...props} />
+            // },
+            code: ({ className, children, ...props }) => {
+              const isInline = children.toString().split('\n').length === 1
+              const match = /language-(\w+)/.exec(className || '')
+              return !isInline ? (
+                <CodeBlock language={match?.[1]} code1={String(children)} {...props} />
+              ) : (
+                <code className={className} {...props}>
+                  {children}
+                </code>
+              )
+            },
+          }}
+        >
+          {markdown}
+        </Markdown>
+      </div>
     </div>
   )
 }
