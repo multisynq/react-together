e305eb1498afb09fa6eb71797db4e025289d404e
afonsocrg <afonso.crg@gmail.com>
2024-08-06 13:59:49 +0100

    update cloudflare build script; moved to scripts dir

--- scripts/cloudflare-build.sh
+++ scripts/cloudflare-build.sh
@@ -20,8 +20,6 @@ trap notify EXIT
 node --version
 npm --version
 
-
-cd ..
 # cloudflare clean-installs arm versions from package-lock.json
 # but actuallty runs on x64 so we need to install the x64 versions
 if [ -n "$CF_PAGES" ]; then
@@ -33,20 +31,12 @@ if [ -n "$CF_PAGES" ]; then
     || exit 1
 else
     echo "Running on local machine, deleting node_modules and clean-installing packages"
-    rm -rf node_modules website/node_modules
+    npm run clean
     npm ci
 fi
 
 # build the react-together package
 npm run build || exit 1
-npm link || exit 1
-
-# build the website
-cd website
-npm ci || exit 1
-npm link react-together || exit 1
-npm run build || exit 1
-
 
 # set RESULT for EXIT trap
 RESULT=SUCCESS
