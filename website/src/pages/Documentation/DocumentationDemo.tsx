a9c83a892fea7aa76c4776e57342dd688d8a061a
afonsocrg <afonso.crg@gmail.com>
2024-08-08 14:20:08 +0100

    rename View to User, in component demos

--- website/src/pages/Documentation/DocumentationDemo.tsx
+++ website/src/pages/Documentation/DocumentationDemo.tsx
@@ -8,10 +8,10 @@ export default function DocumentationDemo({ url }: DocumentationDemoProps) {
   const demoUrl = `/#/demos/${url}`
   return (
     <div className='w-full flex items-center flex-wrap gap-3 justify-center bg-white-100'>
-      <DocumentDemoContainer labelText='View 1'>
+      <DocumentDemoContainer labelText='User 1'>
         <Iframe url={demoUrl} height='100%' width='100%' />
       </DocumentDemoContainer>
-      <DocumentDemoContainer labelText='View 2'>
+      <DocumentDemoContainer labelText='User 2'>
         <Iframe url={demoUrl} height='100%' width='100%' />
       </DocumentDemoContainer>
     </div>
