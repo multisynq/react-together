3f2ddf341df25ac12c4b1721a062abc2ca076aea
elijah lee <84287663+aloha-elijah@users.noreply.github.com>
2024-08-08 12:06:34 -0700

    Pricing Banner Styling update
    Removed Pricing Card and test.svg

--- website/src/components/sections/PricingBanner.tsx
+++ website/src/components/sections/PricingBanner.tsx
@@ -2,6 +2,9 @@ import MSQ_logo from '../../images/MSQ_logo.jpg'
 import Background from '../../images/pricing_background.png'
 
 export function PricingBanner() {
+  const COST_LINK = '/#/pricing'
+  const SYNQER_LINK = 'https://multisynq.io/'
+
   const divStyle = {
     backgroundImage: `url(${Background})`,
     backgroundSize: 'cover',
@@ -12,17 +15,36 @@ export function PricingBanner() {
 
   return (
     <>
-      <div className='w-full h-[20rem] bg-blue-200 sm:bg-red-200 md:bg-green-300 xl:bg-blue-100 relative mt-28 flex items-center justify-center gap-4'>
+      <div className='w-full h-[20rem] bg-blue-200 relative mt-28 flex items-center justify-center gap-4'>
         <div className='w-1/2 h-full flex flex-col justify-center items-end'>
-          <div className='flex flex-col gap-4 pl-4'>
-            <h2 className='' style={{ fontWeight: '700', fontSize: '40px', lineHeight: '1.25' }}>
+          <div className='flex flex-col gap-5 ml-16'>
+            <h2 className='' style={{ fontSize: '38px', fontWeight: '700', lineHeight: '1.25' }}>
               Crazy Affordable Networked,
               <br />
               Multiuser Experiences
             </h2>
-            <p className='max-w-[36rem]'>
-              This project is open source and free. Using the DePIN infra has its costs, but you can earn money from it by being a synqer.
-            </p>
+            <span
+              className='max-w-[36rem] text-gray-700'
+              style={{ fontWeight: '300', fontSize: '18px', lineHeight: '1.25', fontFamily: 'Inter' }}
+            >
+              This project is open source and free. Using the DePIN infra has its{' '}
+              <a
+                href={COST_LINK}
+                onClick={() => window.scrollTo(0, 0)}
+                className='border-b-2 border-blue-700 italic font-medium px-2 hover:border-white hover:bg-blue-50 hover:rounded-lg'
+              >
+                costs
+              </a>
+              , but you can earn money from it by being a{' '}
+              <a
+                href={SYNQER_LINK}
+                target='_blank'
+                className='border-b-2 border-blue-700 italic font-medium px-2 hover:border-white hover:bg-blue-50 hover:rounded-lg'
+              >
+                synqer
+              </a>
+              .
+            </span>
           </div>
         </div>
         <div className='w-1/2 h-full relative'>
