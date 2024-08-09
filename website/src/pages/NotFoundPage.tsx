da9174e0ffb7dc43957cadc823bd920304f1fabc
elijah lee <84287663+aloha-elijah@users.noreply.github.com>
2024-08-08 21:45:21 -0700

    404 page with image

--- website/src/pages/NotFoundPage.tsx
+++ website/src/pages/NotFoundPage.tsx
@@ -1,11 +1,13 @@
 // import Link from '@components/ui/Link'
-
 import { useNavigate } from 'react-router-dom'
+import notFoundImage from '../images/pageNotFound.png'
 
 export function NotFoundPage() {
   const navigate = useNavigate()
   return (
-    <div className='text-center'>
+    <div className='flex justify-center items-center flex-col gap-6 bg-blue-50 h-[80vh]'>
+      {/* <div className='h-[20rem] w-[40rem] bg-blue-300' /> */}
+      <img src={notFoundImage} alt='404 Page Not found with compass' className='w-auto h-[12rem] rounded-lg' />
       <h3>Uh oh, this page has gone on an adventure.</h3>
 
       <div
