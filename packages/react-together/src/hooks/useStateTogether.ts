cd90c50dcb04a01770e2e73027bd423c1926b2ab
afonsocrg <afonso.crg@gmail.com>
2024-09-02 12:53:03 +0100

    useStateTogether handles session transitions smoothly

--- packages/react-together/src/hooks/useStateTogether.ts
+++ packages/react-together/src/hooks/useStateTogether.ts
@@ -1,76 +1,68 @@
-import { CroquetContext, Session, View } from '@croquet/react'
+import { useCroquetContext } from '@croquet/react'
 import {
   Dispatch,
   SetStateAction,
   useCallback,
-  useContext,
   useEffect,
   useState
 } from 'react'
 import ReactTogetherModel from '../models/ReactTogetherModel'
 import getNewValue from './getNewValue'
 
+interface UseStateTogetherOptions {}
+
 export default function useStateTogether<T>(
   rtKey: string,
-  initial_value: T,
+  initialValue: T,
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  _options?: Record<string, unknown>
+  _options: UseStateTogetherOptions = {}
 ): [T, Dispatch<SetStateAction<T>>] {
-  // If we are in a session, return the shared state
-  // otherwise, act as a normal useState
+  // If a session is active, it uses the shared state from the session by
+  // subscribing to updates and publishing changes.
+  // If no session is active, it behaves like a normal useState hook,
+  // maintaining local state only.
 
-  // Accessing CroquetContext directly because
-  // we may not be inside a Croquet session
-  const context = useContext(CroquetContext)
-  let model: null | ReactTogetherModel = null
-  let session: null | Session = null
-  let view: null | View = null
-  const insideCroquetSession = context !== undefined
-  if (insideCroquetSession) {
-    model = context.model as ReactTogetherModel
-    session = context.session
-    view = context.view
-  }
-  const modelValue = model?.state.get(rtKey) as T
+  const { session, view, model } = useCroquetContext<ReactTogetherModel>()
 
-  // This is the local state
   const [value, set_value] = useState<T>(() => {
-    // This function is only executed on the first render
-    // If there is no value for this key, publish the initial
-    // value
-    if (view && model && !model.state.has(rtKey)) {
-      view.publish(model.id, 'setState', { id: rtKey, newValue: initial_value })
-      return initial_value
+    // This function runs only during the initial render.
+    // If the key does not exist in the model's state, it publishes the initial value.
+    // Otherwise, it retrieves and returns the existing value from the model's state.
+    if (!view || !model) return initialValue
+    if (!model.state.has(rtKey)) {
+      view.publish(model.id, 'setState', { id: rtKey, newValue: initialValue })
+      return initialValue
     }
-    return modelValue
+    return model.state.get(rtKey) as T
   })
 
   useEffect(() => {
     if (!session || !view || !model) return
 
     const handler = () => {
-      const newValue = model.state.get(rtKey) as T
-      set_value((prev) => (prev === newValue ? prev : newValue))
+      set_value((prev) => {
+        if (!model.state.has(rtKey)) {
+          view.publish(model.id, 'setState', { id: rtKey, newValue: prev })
+          return prev
+        }
+        const newValue = model.state.get(rtKey) as T
+        return prev === newValue ? prev : newValue
+      })
     }
     view.subscribe(
       rtKey,
       { event: 'updated', handling: 'oncePerFrame' },
       handler
     )
-    return () => {
-      try {
-        view.unsubscribe(rtKey, 'updated', handler)
-      } catch (error) {
-        console.error('Failed to unsubscribe:', error)
-      }
-    }
+    handler()
+    return () => view.unsubscribe(rtKey, 'updated', handler)
   }, [session, view, model, rtKey, set_value])
 
   const setter = useCallback(
     (newValueOrFn: SetStateAction<T>): void => {
       if (model && view) {
         // Eventually we will want to throttle publish calls
-        const oldValue = model.state.get(rtKey)! as T
+        const oldValue = model.state.get(rtKey) as T
         view.publish(model.id, 'setState', {
           id: rtKey,
           newValue: getNewValue(oldValue, newValueOrFn)
