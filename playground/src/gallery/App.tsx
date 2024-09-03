4cefa55f40248b00ad290dc448945345b82ccd3b
afonsocrg <afonso.crg@gmail.com>
2024-09-03 14:06:23 +0100

    [playground] add default session to gallery demo

--- playground/src/gallery/App.tsx
+++ playground/src/gallery/App.tsx
@@ -5,6 +5,8 @@ export default function App() {
   return (
     <ReactTogether
       sessionParams={{
+        name: 'Gallery Default session',
+        password: 'super-secret-password',
         appId: import.meta.env['VITE_APP_ID'],
         apiKey: import.meta.env['VITE_API_KEY']
       }}
