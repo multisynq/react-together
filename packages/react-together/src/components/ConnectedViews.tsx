aaac95c117140bcf514d25ca92e3872ff5472ec5
afonsocrg <afonso.crg@gmail.com>
2024-09-16 12:37:51 +0100

    update connectedViews to display avatar instead of +1

--- packages/react-together/src/components/ConnectedViews.tsx
+++ packages/react-together/src/components/ConnectedViews.tsx
@@ -2,6 +2,21 @@ import { Avatar } from 'primereact/avatar'
 import { AvatarGroup } from 'primereact/avatargroup'
 import { useConnectedViews } from '../hooks'
 
+interface ViewAvatarProps {
+  name: string
+}
+function ViewAvatar({ name }: ViewAvatarProps) {
+  return (
+    <Avatar
+      image={`https://api.dicebear.com/8.x/initials/svg?seed=${name}`}
+      size="normal"
+      shape="circle"
+      label={name}
+      pt={{ root: { title: name } }}
+    />
+  )
+}
+
 type ConnectedViewsProps = {
   maxAvatars?: number
   debug?: boolean
@@ -15,24 +30,20 @@ export default function ConnectedViews({
   if (!users) return
 
   const size = 'normal'
-  const nAvatars = Math.max(users.length - maxAvatars, 0)
+  const nAvatars = Math.max(users.length - (maxAvatars - 1), 0)
 
   return (
     <>
       <AvatarGroup pt={{ root: { style: { gap: '10px' } } }}>
-        {users.slice(0, maxAvatars).map(({ name, viewId }) => (
-          <Avatar
-            key={viewId}
-            image={`https://api.dicebear.com/8.x/initials/svg?seed=${name}`}
-            size={size}
-            shape="circle"
-            label={name}
-            pt={{ root: { title: name } }}
-          />
+        {users.slice(0, maxAvatars - 1).map(({ name, viewId }) => (
+          <ViewAvatar key={viewId} name={name} />
         ))}
-        {nAvatars > 0 && (
-          <Avatar label={`+${nAvatars}`} shape="circle" size={size} />
-        )}
+        {nAvatars > 0 &&
+          (nAvatars === 1 ? (
+            <ViewAvatar name={users[users.length - 1].name} />
+          ) : (
+            <Avatar label={`+${nAvatars}`} shape="circle" size={size} />
+          ))}
       </AvatarGroup>
       {debug && (
         <div style={{ textAlign: 'left' }}>
