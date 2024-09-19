9c895eee45664bc45457e22433e6a7354bc87bf5
elijah lee <84287663+aloha-elijah@users.noreply.github.com>
2024-09-19 12:16:49 -0700

    Responsiveness updatre

--- website/src/components/hacktogether/CoverBanner.tsx
+++ website/src/components/hacktogether/CoverBanner.tsx
@@ -5,18 +5,18 @@ import { HashLink } from 'react-router-hash-link'
 
 // Helper Components
 const EventContentHorizontal = () => (
-  <div className='w-full h-full bg-indigo-600 flex text-white px-8 py-4 items-center justify-center border rounded-xl border-gray-800 shadow-lineStyleDark'>
-    <div className='flex w-full justify-between'>
-      <div className='w-[6rem]'>
-        <span className='text-lg font-medium font-poppins'>November, 9-10 2024</span>
+  <div className='w-full h-full bg-indigo-600 flex text-white px-1 py-4 items-center justify-center border rounded-xl border-gray-800 shadow-lineStyleDark md:bg-cyan-500'>
+    <div className='flex w-full items-center justify-between px-[0.5rem] max-w-[20rem] md:hidden'>
+      <div className='w-full'>
+        <span className='text-sm font-medium font-poppins text-center'>Nov, 9-10 2024</span>
       </div>
       <Divider layout='vertical' />
-      <div className='w-[6rem]'>
-        <span className='text-lg font-medium font-poppins'>Lisbon, Portugal</span>
+      <div className='w-full'>
+        <span className='text-sm font-medium font-poppins text-center'>Lisbon, Portugal</span>
       </div>
       <Divider layout='vertical' />
-      <div className='w-[6rem]'>
-        <span className='text-lg font-medium font-poppins'>
+      <div className='w-full text-center'>
+        <span className='text-sm font-medium font-poppins'>
           $4,000
           <br />
           in prizes
@@ -54,22 +54,22 @@ const EventContentVertical = () => (
 
 // Main Components
 const CenterRow = () => (
-  <div className='w-full h-full flex gap-3'>
+  <div className='w-full h-full flex gap-3 '>
     <div className='flex w-full flex-col h-full border-black gap-3'>
       {/* Main Content */}
       <div className='w-full h-full bg-rose-500 flex px-8 py-6 flex-col justify-end rounded-xl border border-gray-800 shadow-lineStyleDark text-white'>
-        <div className='w-[16rem] flex flex-col md:w-[22rem]'>
+        <div className='w-[14rem] flex flex-col md:w-[22rem]'>
           <div className='flex flex-col'>
-            <span className='text-[3rem] sm:text-[4rem] md:text-[5rem] font-thin tracking-tighter font-poppins leading-[0.9] md:leading-[0.8]'>
+            <span className='text-[2.5rem] sm:text-[4rem] md:text-[5rem] font-thin tracking-tighter font-poppins leading-[0.9] md:leading-[0.8]'>
               hack
             </span>
-            <span className='text-[3rem] sm:text-[4rem] md:text-[5rem] tracking-tighter font-semibold font-poppins leading-[0.8] md:leading-[1.0]'>
+            <span className='text-[2.5rem] sm:text-[4rem] md:text-[5rem] tracking-tighter font-semibold font-poppins leading-[0.8] md:leading-[1.0]'>
               Together
             </span>
           </div>
           <Divider />
           <div>
-            <span className='text-2xl font-poppins font-light tracking-tight leading-[0.8]'>
+            <span className='text-lg font-poppins font-light tracking-tight leading-[0.8] sm:text-xl'>
               Join us building the
               <br /> web of tomorrow.
             </span>
@@ -92,9 +92,8 @@ const CenterRow = () => (
 
     {/* Side Column (desktop) */}
     <div className='w-[16rem] hidden sm:flex flex-col gap-3'>
-      <div className='hidden w-full h-[12rem] bg-green-200 rounded-xl border border-gray-800 shadow-lineStyleDark lg:block' />
-      <div className='w-full h-[14rem] md:h-full bg-yellow-200 rounded-xl border border-gray-800 shadow-lineStyleDark' />
-      <div className='h-full w-full md:hidden'>
+      <div className='w-full h-[16rem] bg-yellow-200 rounded-xl border border-gray-800 shadow-lineStyleDark' />
+      <div className='h-full w-full'>
         <EventContentVertical />
       </div>
       <div className='w-full h-[5rem] bg-blue-300 hidden md:block overflow-hidden rounded-xl border border-gray-800 shadow-lineStyleDark'>
@@ -108,7 +107,7 @@ const CenterRow = () => (
 
 // Main Component
 const CoverBanner = () => (
-  <div className='w-full flex items-center gap-3 border-b h-[42rem] md:h-[50rem] border-gray-800 pt-[1rem] pb-[1rem] md:pt-[2rem] md:pb-[4rem] bg-neutral-50'>
+  <div className='w-full flex items-center gap-3 border-b h-[32rem] sm:h-[40rem] md:h-[50rem] border-gray-800 pt-[1rem] pb-[1rem] md:pt-[2rem] md:pb-[4rem] bg-neutral-50'>
     {/* Left Column */}
     <div className='w-3 h-full flex flex-col gap-3 md:w-full'>
       <div className='border border-l-0 border-gray-800 shadow-lineStyleDark rounded-tl-none rounded-tr-xl rounded-br-xl rounded-bl-none w-full h-full bg-cyan-100' />
