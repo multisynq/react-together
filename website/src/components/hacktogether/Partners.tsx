9d19273a46537dd357b3ce82754f8e03ae201e88
afonsocrg <afonso.crg@gmail.com>
2024-09-30 20:57:01 +0100

    add currently confirmed partners

--- website/src/components/hacktogether/Partners.tsx
+++ website/src/components/hacktogether/Partners.tsx
@@ -1,12 +1,17 @@
+import multisynqLogo from '@images/blue.png'
+import gamingHubLogo from '@images/gaming_hub_logo.jpg'
+import hackerSchoolLogo from '@images/hackerschool_logo.png'
+import neetiLogo from '@images/neeti_logo.jpeg'
+
 interface PartnerIconProps {
-  url?: string
   src: string
-  alt: string
+  url?: string
+  alt?: string
 }
 
 function PartnerIcon({ url, src, alt }: PartnerIconProps) {
   return (
-    <a href={url} target='_blank' rel='noopener noreferrer'>
+    <a href={url} target='_blank' rel='noopener noreferrer' className={url === null ? 'cursor-auto' : 'cursor-pointer'}>
       <div className='flex items-center justify-center border border-gray-800 bg-white rounded-xl w-full px-[4rem] py-[1.5rem] shadow-lineStyleDark max-w-[34rem] '>
         <div className='flex h-[6rem] w-full'>
           <div className='w-full h-full flex items-center justify-center'>
@@ -18,18 +23,48 @@ function PartnerIcon({ url, src, alt }: PartnerIconProps) {
   )
 }
 
-// const partners = [
-// ]
+const partners = [
+  // Not sure where to put Multisynq logo... Having a section just for us seems a little bit weird... Maybe Bounty Partners?
+  {
+    src: multisynqLogo,
+    url: 'https://multisynq.io',
+  },
+  // Host partner
+  // {
+  //   src: null, // We need to find a horizontal logo...
+  //   url: 'https://www.hood.pt/',
+  // },
+  // Community Partners
+]
+
+const communityPartners = [
+  {
+    src: neetiLogo,
+    url: 'https://neeti.tecnico.ulisboa.pt/',
+  },
+  {
+    src: hackerSchoolLogo,
+    url: 'https://linktr.ee/hackerschool_ist',
+  },
+  {
+    src: gamingHubLogo,
+    url: null,
+  },
+  // PENDING:
+  // BUL, SULX, Startup Portugal, Women in Tech Portugal, NEIIST, GameDev, 42 Lisbon, LeWagon, In-Nova, Novae, JEs todas
+]
 
 export default function Partners() {
   return (
     <>
-      {/* <div className='flex flex-col align-items-center justify-content-between gap-8'>
+      <div className='flex flex-col align-items-center justify-content-between gap-8'>
         {partners.map((p) => (
-          <PartnerIcon key={p.url} url={p.url} src={p.img} alt={p.alt} />
+          <PartnerIcon key={p.url} url={p.url} src={p.src} />
         ))}
-      </div> */}
-      <h3>Partners will be announced soon. Stay tuned!!</h3>
+        {communityPartners.map((p) => (
+          <PartnerIcon key={p.url} url={p.url} src={p.src} />
+        ))}
+      </div>
 
       <div className='text-center text-sm mt-8'>
         Interested in partnering?{' '}
