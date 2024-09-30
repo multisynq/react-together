dd45f84287469e0d8d39e25e74ffdcbecb577282
elijah lee <84287663+aloha-elijah@users.noreply.github.com>
2024-09-30 14:24:31 -0700

    Update VenueInfo.tsx

--- website/src/components/hacktogether/VenueInfo.tsx
+++ website/src/components/hacktogether/VenueInfo.tsx
@@ -1,23 +1,31 @@
+import 'primeicons/primeicons.css'
 import ht_image from '../../images/ht_CharlesSquare.jpg'
 
 const MAP_URL_LINK = 'https://maps.app.goo.gl/PccWHKChX58e1ur67'
 
 export default function VenueInfo() {
+  const handleMapClick = () => {
+    window.open(MAP_URL_LINK, '_blank') // Opens the link in a new tab
+  }
+
   return (
-    <div className='flex flex-col-reverse sm:flex-row items-center justify-center px-3 gap-4 w-full sm:items-start'>
-      <div className='w-full sm:w-2/3 bg-slate-200 aspect-[4/3] rounded-xl border border-gray-800 shadow-lineStyleDark overflow-hidden'>
+    <div className='flex flex-col sm:flex-row items-center justify-center px-3 gap-4 w-full sm:items-start'>
+      <div className='w-full sm:w-2/3 aspect-[16/9] rounded-xl border border-gray-800 shadow-lineStyleDark overflow-hidden'>
         <img src={ht_image} alt='Multisynq Logo' className='w-full h-auto rounded-lg' />
       </div>
-      <button
-        className='flex flex-col w-full sm:w-1/3 px-4 pb-3 items-start border border-gray-800 rounded-xl shadow-lineStyleDark bg-black text-white hover:bg-gray-800 sm:mt-[2rem]'
-        onClick={() => window.open(MAP_URL_LINK)}
-      >
-        <h5 style={{ fontSize: '40px', fontFamily: 'poppins' }}>Hood</h5>
-        <span className='font-light text-xl text-start px-2'>
-          Rua 3 da Matinha, Edifício Altejo
-          <br /> 101 - 1950-326 Lisboa
-        </span>
-      </button>
+      <div className='flex w-full sm:w-1/3 px-4 pb-3 sm:mt-[2rem gap-2 sm:flex-col'>
+        <h5 style={{ fontSize: '40px', fontFamily: 'poppins', margin: '8px' }}>Hood</h5>
+        <button className='text-start flex gap-2 m-2' onClick={handleMapClick}>
+          <span className='font-light text-xl text-gray-950 hover:text-gray-600 leading-tight mt-4' style={{ fontFamily: 'poppins' }}>
+            <i className='pi pi-map text-xl mr-2'></i>
+            Rua 3 da Matinha
+            <br />
+            Edifício Altejo
+            <br />
+            101 - 1950-326 Lisboa
+          </span>
+        </button>
+      </div>
     </div>
   )
 }
