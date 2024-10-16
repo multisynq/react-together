94cdc4a1bf41a169859afce4929ffa0cdeeb65b9
elijah lee <84287663+aloha-elijah@users.noreply.github.com>
2024-10-16 09:16:50 -0700

    [website] initial reactTogether Manger Demo
    -added components from the playground
    -change min width for demo container

--- website/src/components/demo/ReactTogetherManagerDemo.tsx
+++ website/src/components/demo/ReactTogetherManagerDemo.tsx
@@ -1,3 +1,52 @@
+import { SetStateAction, useState } from 'react'
+import { ReactTogetherManager } from 'react-together'
+import CountButtonTogether from './CountButtonTogether'
+
+function UrlContainer() {
+  const [url, setUrl] = useState('https://current-address.com/294spwd') // Replace with your dynamic URL
+
+  function handleUrlChange(e: { target: { value: SetStateAction<string> } }) {
+    setUrl(e.target.value)
+  }
+
+  function handleKeyDown(e: React.KeyboardEvent<HTMLInputElement>) {
+    if (e.key === 'Enter') {
+      handleSummit()
+    }
+  }
+  function handleSummit() {
+    console.log('Summit')
+  }
+
+  return (
+    <div className='flex align-items-center gap-2 border rounded-xl border-gray-800 items-center py-2 px-4 w-full justify-between shadow-lineStyleDark hover:shadow-lineStyleMedium'>
+      <input
+        type='text'
+        value={url}
+        onChange={handleUrlChange}
+        className='w-full bg-transparent border-none outline-none'
+        onClick={(e) => (e.target as HTMLInputElement).select()}
+        onKeyDown={handleKeyDown}
+        onBlur={handleSummit}
+      />
+      <button onClick={handleSummit}>
+        <i className='pi pi-arrow-right'></i>
+      </button>
+    </div>
+  )
+}
+
 export default function ReactTogetherManagerDemo() {
-  return <div>ReactTogetherMangerDemo</div>
+  return (
+    <div className='h-full w-full relative flex justify-center'>
+      <div className='flex flex-col gap-8 p-4 items-center'>
+        <UrlContainer />
+        <CountButtonTogether />
+        <div />
+      </div>
+      <div className='absolute bottom-2 left-2'>
+        <ReactTogetherManager />
+      </div>
+    </div>
+  )
 }
--- website/src/components/ui/DocumentDemoContainer.tsx
+++ website/src/components/ui/DocumentDemoContainer.tsx
@@ -2,7 +2,7 @@ export function DocumentDemoContainer({ children, labelText }) {
   return (
     <>
       <div
-        className='line-border bg-white flex items-center justify-center flex-1 relative bg-slate-100 overflow-auto min-w-[200px]'
+        className='line-border flex items-center justify-center flex-1 relative bg-slate-100 overflow-auto min-w-[18rem]'
         style={{ aspectRatio: '5 / 3' }}
       >
         <div className='rounded-xl absolute py-1 px-2 justify-center items-center border-2 border-gray-500 bottom-2 shadow-lineStyleMedium right-2 bg-blue-100'>
