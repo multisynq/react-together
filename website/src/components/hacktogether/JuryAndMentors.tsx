c88f5b8c8c04d4fe3c91ab71da4750e0997e3e7f
afonsocrg <afonso.crg@gmail.com>
2024-10-19 11:13:04 +0100

    add Maxim to mentors

--- website/src/components/hacktogether/JuryAndMentors.tsx
+++ website/src/components/hacktogether/JuryAndMentors.tsx
@@ -2,6 +2,7 @@ import AranLunzer from '@images/people/AranLunzer.jpeg'
 import DavidSmith from '@images/people/DavidSmith.jpeg'
 import JohnPayne from '@images/people/JohnPayne.jpeg'
 import KenLane from '@images/people/KenLane.jpeg'
+import MaximLegg from '@images/people/MaximLegg.png'
 import MiguelMatos from '@images/people/MiguelMatos.jpeg'
 import VanessaFreudenberg from '@images/people/VanessaFreudenberg.jpeg'
 import YuliiaBilyk from '@images/people/YuliiaBilyk.jpeg'
@@ -26,6 +27,12 @@ const people: Person[] = [
     img: DavidSmith,
     company: 'Multisynq.io',
     profileUrl: 'https://www.linkedin.com/in/croquet',
+  },
+  {
+    name: 'Maxim Legg',
+    img: MaximLegg,
+    company: 'Pangea',
+    profileUrl: 'https://www.linkedin.com/in/maximlegg/',
   },
   {
     name: 'Vanessa Freudenberg',
