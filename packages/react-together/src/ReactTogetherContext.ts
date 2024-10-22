584f93e544867fa2680af9b6b1fa6bce467d5517
afonsocrg <afonso.crg@gmail.com>
2024-10-22 15:40:14 +0100

    removed unused Context

--- packages/react-together/src/ReactTogetherContext.ts
+++
@@ -1,9 +0,0 @@
-import { createContext } from 'react'
-
-export interface ReactTogetherContext {
-  createNewSession: () => void
-}
-
-export const ReactTogetherContext = createContext<
-  ReactTogetherContext | undefined
->(undefined)
