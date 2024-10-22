689848cc4e00e05dfe03c8125b968c4f94f985b4
afonsocrg <afonso.crg@gmail.com>
2024-10-22 15:18:27 +0100

    implement SessionManagerDemo logic

--- website/src/components/demo/SessionManagerDemo.tsx
+++ website/src/components/demo/SessionManagerDemo.tsx
@@ -1,35 +1,46 @@
-import { SetStateAction, useState } from 'react'
-import { SessionManager } from 'react-together'
+import { useCallback, useEffect, useState } from 'react'
+import { CroquetReact, SessionManager, useIsTogether, useJoinUrl } from 'react-together'
 import CountButtonTogether from './CountButtonTogether'
 
-function UrlContainer() {
-  const [url, setUrl] = useState('https://current-address.com/294spwd') // Replace with your dynamic URL
+const { useSessionParams } = CroquetReact
 
-  function handleUrlChange(e: { target: { value: SetStateAction<string> } }) {
-    setUrl(e.target.value)
+interface UrlContainerProps {
+  localUrl: string
+  setLocalUrl: (newUrl: string) => void
+  onSubmit: () => void
+}
+function UrlContainer({ localUrl, setLocalUrl, onSubmit }: UrlContainerProps) {
+  const [previousUrl, setPreviousUrl] = useState(localUrl)
+
+  function handleUrlChange(e: React.ChangeEvent<HTMLInputElement>) {
+    setLocalUrl(e.target.value)
+  }
+
+  const handleSubmit = () => {
+    setPreviousUrl(localUrl)
+    onSubmit()
   }
 
   function handleKeyDown(e: React.KeyboardEvent<HTMLInputElement>) {
     if (e.key === 'Enter') {
-      handleSummit()
+      handleSubmit()
     }
-  }
-  function handleSummit() {
-    console.log('Summit')
+    if (e.key === 'Escape') {
+      setLocalUrl(previousUrl)
+    }
   }
 
   return (
     <div className='flex align-items-center gap-2 border rounded-xl border-gray-800 items-center py-2 px-4 w-full justify-between shadow-lineStyleDark hover:shadow-lineStyleMedium'>
       <input
         type='text'
-        value={url}
+        value={localUrl ?? ''}
         onChange={handleUrlChange}
         className='w-full bg-transparent border-none outline-none'
         onClick={(e) => (e.target as HTMLInputElement).select()}
         onKeyDown={handleKeyDown}
-        onBlur={handleSummit}
       />
-      <button onClick={handleSummit}>
+      <button onClick={handleSubmit}>
         <i className='pi pi-arrow-right'></i>
       </button>
     </div>
@@ -37,14 +48,47 @@ function UrlContainer() {
 }
 
 export default function SessionManagerDemo() {
+  const joinUrl = useJoinUrl()
+
+  // Keep local URL up to date with joinUrl
+  const [localUrl, setLocalUrl] = useState(joinUrl ?? window.location.href)
+
+  const isTogether = useIsTogether()
+  const { name } = useSessionParams()
+
+  useEffect(() => {
+    setLocalUrl(joinUrl ?? window.location.href)
+  }, [joinUrl, setLocalUrl])
+
+  const onSubmit = useCallback(() => {
+    try {
+      // Assert that users only change the searchParams
+      const newSearchParams = new URL(localUrl).searchParams
+      const newUrl = new URL(window.location.href)
+      newSearchParams.forEach((value, key) => {
+        newUrl.searchParams.set(key, value)
+      })
+      setLocalUrl(newUrl.toString())
+
+      window.location.assign(newUrl)
+    } catch (e) {
+      console.error(`${localUrl} is not a valid URL`)
+      setLocalUrl(joinUrl ?? window.location.href)
+    }
+  }, [localUrl, setLocalUrl, joinUrl])
+
   return (
     <div className='h-full w-full relative flex justify-center'>
-      <div className='flex flex-col gap-8 p-4 items-center'>
-        <UrlContainer />
+      <div className='flex w-full flex-col gap-8 p-4 items-center'>
+        <UrlContainer {...{ localUrl, setLocalUrl, onSubmit }} />
         <CountButtonTogether />
-        <div />
+        <p className='text-xs px-12'>
+          {isTogether
+            ? `You are in session "${name}". You can invite your friends by sending them the URL in the bar above!`
+            : `You are currently disconnected. Paste a Join URL in the bar above, or click on the button below to create a private session!`}
+        </p>
       </div>
-      <div className='absolute bottom-2 left-2'>
+      <div className='fixed bottom-2 left-2'>
         <SessionManager />
       </div>
     </div>
--- website/src/pages/Documentation/DocumentationDemo.tsx
+++ website/src/pages/Documentation/DocumentationDemo.tsx
@@ -1,18 +1,50 @@
 import { DocumentDemoContainer } from '@components/ui/DocumentDemoContainer'
 import Iframe from 'react-iframe'
+import { SESSION_NAME_PARAM, SESSION_PASSWORD_PARAM } from 'react-together'
+
+function buildUrl(path: string, session: SessionParams): string {
+  const params = new URLSearchParams([
+    [SESSION_NAME_PARAM, session.name],
+    [SESSION_PASSWORD_PARAM, session.password],
+  ])
+  // If the search parameters are after the hash,
+  // they will be treated as part of the hash
+  return `?${params.toString()}#${path}`
+}
+
+const defaultSession: SessionParams = Object.freeze({
+  name: 'ReactTogetherDemo',
+  password: 'secret-password',
+})
+
+interface SessionParams {
+  name?: string
+  password?: string
+}
 
 interface DocumentationDemoProps {
   url: string
+  session1?: SessionParams | null
+  session2?: SessionParams | null
 }
-export default function DocumentationDemo({ url }: DocumentationDemoProps) {
-  const demoUrl = `/#/demos/${url}`
+export default function DocumentationDemo({ url, session1, session2 }: DocumentationDemoProps) {
+  const demoPath = `/demos/${url}`
+  if (session1 === undefined) {
+    session1 = defaultSession
+  }
+  if (session2 === undefined) {
+    session2 = session1
+  }
+  const url1 = buildUrl(demoPath, session1)
+  const url2 = buildUrl(demoPath, session2)
+
   return (
     <div className='w-full flex items-center flex-wrap gap-3 justify-center bg-white-100'>
       <DocumentDemoContainer labelText='User 1'>
-        <Iframe url={demoUrl} height='100%' width='100%' />
+        <Iframe url={url1} height='100%' width='100%' />
       </DocumentDemoContainer>
       <DocumentDemoContainer labelText='User 2'>
-        <Iframe url={demoUrl} height='100%' width='100%' />
+        <Iframe url={url2} height='100%' width='100%' />
       </DocumentDemoContainer>
     </div>
   )
