56ff4a132d75d3a814246273593699754797e37a
afonsocrg <afonso.crg@gmail.com>
2024-10-22 14:55:37 +0100

    tidy react-together exports

--- packages/react-together/src/index.ts
+++ packages/react-together/src/index.ts
@@ -1,74 +1,8 @@
-import ReactTogetherModel from './models/ReactTogetherModel'
+// Re-export @croquet/react as is into CroquetReact
+// export type { CroquetSession, CroquetSessionParameters } from '@croquet/react'
+import * as CroquetReact from '@croquet/react'
+export { CroquetReact }
 
-// Re-export from @croquet/react -> into Croquet object
-import {
-  App,
-  Constants,
-  CroquetReactView,
-  Data,
-  Model,
-  ReactModel,
-  Session,
-  View,
-  createCroquetSession,
-  // Not exporting this hook since we override it on react-together
-  // useJoinedViews,
-  useCroquetContext,
-  useDetachCallback,
-  useIsJoined,
-  useLeaveSession,
-  useModelById,
-  useModelRoot,
-  useModelSelector,
-  usePublish,
-  useReactModelRoot,
-  useSession,
-  useSessionId,
-  useSetSession,
-  useSubscribe,
-  useSyncedCallback,
-  useUpdateCallback,
-  useView,
-  useViewId
-} from '@croquet/react'
-
-export const Croquet = {
-  App,
-  Constants,
-  CroquetReactView,
-  Data,
-  Model,
-  ReactModel,
-  Session,
-  View,
-  createCroquetSession,
-  // Not exporting this hook since we override it on react-together
-  // useJoinedViews,
-  useCroquetContext,
-  useDetachCallback,
-  useIsJoined,
-  useModelById,
-  useModelRoot,
-  useLeaveSession,
-  useModelSelector,
-  usePublish,
-  useReactModelRoot,
-  useSession,
-  useSessionId,
-  useSetSession,
-  useSubscribe,
-  useSyncedCallback,
-  useUpdateCallback,
-  useView,
-  useViewId
-}
-
-export type { CroquetSession, CroquetSessionParameters } from '@croquet/react'
 export * from './components'
 export * from './hooks'
-export { ReactTogetherModel, useLeaveSession }
-
-// Create hook alias
-const useIsTogether = useIsJoined
-const useMyId = useViewId
-export { useIsTogether, useMyId }
+export * from './models'
