85a1b3fd8752448e31640940eab038def9a69fed
afonsocrg <afonso.crg@gmail.com>
2024-10-23 21:22:56 +0100

    redirect hash router paths to normal paths

--- website/main.tsx
+++ website/main.tsx
@@ -3,6 +3,15 @@ import { createRoot } from 'react-dom/client'
 
 import App from '@App'
 
+const hashPath = window.location.hash
+if (hashPath.startsWith('#/')) {
+  // Routes:
+  const newUrl = new URL(window.location.href)
+  newUrl.hash = ''
+  newUrl.pathname = hashPath.slice(1)
+  window.history.replaceState({}, '', newUrl)
+}
+
 const container = document.getElementById('root')
 createRoot(container!).render(
   <StrictMode>
