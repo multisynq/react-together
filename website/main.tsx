3d48d6c59d921694e217a7b30dc8ec1d0330871c
afonsocrg <afonso.crg@gmail.com>
2024-10-23 21:38:27 +0100

    refactor code that transforms hash-pathed urls to normal urls

--- website/main.tsx
+++ website/main.tsx
@@ -3,13 +3,15 @@ import { createRoot } from 'react-dom/client'
 
 import App from '@App'
 
-const hashPath = window.location.hash
-if (hashPath.startsWith('#/')) {
-  // Routes:
-  const newUrl = new URL(window.location.href)
-  newUrl.hash = ''
-  newUrl.pathname = hashPath.slice(1)
-  window.history.replaceState({}, '', newUrl)
+// Previously we were using a hashrouter, but now we are using a regular one
+// This code change urls that have their path on the url hash so that the
+// path is set properly.
+// (There are link on the internet that are using the hash path)
+const url = new URL(window.location.href)
+if (url.pathname === '/' && url.hash.startsWith('#/')) {
+  url.pathname = url.hash.slice(1)
+  url.hash = ''
+  window.history.replaceState({}, '', url)
 }
 
 const container = document.getElementById('root')
