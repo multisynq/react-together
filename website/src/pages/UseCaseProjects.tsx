3504f43c0169100030e6dde514afec7360f0cdd9
elijah lee <84287663+aloha-elijah@users.noreply.github.com>
2024-10-30 21:00:17 -0700

    [website] Use Case project page layout

--- website/src/pages/UseCaseProjects.tsx
+++ website/src/pages/UseCaseProjects.tsx
@@ -1,7 +1,29 @@
+function TempCardHolder() {
+  return (
+    <div className='aspect-video w-full bg-slate-300 rounded-lg flex items-center justify-center'>
+      <h3>Project Card</h3>
+    </div>
+  )
+}
+
 export function UseCaseProject() {
   return (
     <div className='w-full bg-blue-50'>
-      <div>UseCase</div>
+      <div className='w-full flex justify-center flex-col items-center'>
+        <div className='h-[24rem] w-full bg-blue-200 px-[4rem] py-[4rem]'>
+          <div className='flex flex-col gap-2'>
+            <span>Use Case</span>
+            <h2>Developed with React Together</h2>
+          </div>
+        </div>
+        <div className='w-full px-[2rem] sm:px-[2rem] md:px-[4rem] max-w-[84rem] flex items-center justify-center py-[3rem] flex-col'>
+          <div className='grid gap-4 w-full grid-cols-1 sm:grid-cols-2 xl:grid-cols-3'>
+            {Array.from({ length: 12 }).map((_, index) => (
+              <TempCardHolder key={index} />
+            ))}
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
