6d0a250e7b3f090ceac4ab8f3fa76f522ce8322d
afonsocrg <afonso.crg@gmail.com>
2024-11-13 21:55:31 +0000

    remove countdown from website

--- website/src/pages/HackTogetherPage.tsx
+++ website/src/pages/HackTogetherPage.tsx
@@ -2,7 +2,6 @@ import CoverBanner from '@components/hacktogether/CoverBanner'
 import EventSchedule from '@components/hacktogether/EventSchedule'
 import FAQTable from '@components/hacktogether/FAQTable'
 import InfoTable from '@components/hacktogether/InfoTable'
-import IntroBanner from '@components/hacktogether/IntroBanner'
 import JuriAndMentors from '@components/hacktogether/JuryAndMentors'
 import Partners from '@components/hacktogether/Partners'
 import VenueInfo from '@components/hacktogether/VenueInfo'
@@ -21,8 +20,6 @@ export function HackTogetherPage() {
       <CoverBanner />
       <div className='w-full flex justify-center'>
         <div className='w-full  px-[1rem] sm:px-[2rem] md:px-[4rem] lg:px-[0rem] max-w-[60rem] flex items-center justify-center py-[3rem] flex-col'>
-          <SubHeader title='Build the web of tomorrow' />
-          <IntroBanner />
           <SubHeader title='About the event ...' />
           <InfoTable />
           <SubHeader title='Judges, Speakers & Mentors' />
