68fac72e530078ce2e1686425112abe0de295f14
elijah lee <84287663+aloha-elijah@users.noreply.github.com>
2024-12-10 14:18:42 -0800

    [website] Preview PreviewSourceCodeTabs Revision

--- website/src/pages/Documentation/PreviewSourceCodeTabs.tsx
+++ website/src/pages/Documentation/PreviewSourceCodeTabs.tsx
@@ -6,9 +6,12 @@ interface PreviewSourceCodeTabsProps {
 }
 export function PreviewSourceCodeTabs({ preview, code }: PreviewSourceCodeTabsProps) {
   return (
-    <TabView>
-      <TabPanel header='Preview'>{preview}</TabPanel>
-      <TabPanel header='Code'>{code}</TabPanel>
-    </TabView>
+    <div className='w-full'>
+      {/* <TabView pt={{ panelContainer: { className: 'px-0' }, inkbar: { style: { borderColor: 'red' } } }}> */}
+      <TabView pt={{}}>
+        <TabPanel header='Preview'>{preview}</TabPanel>
+        <TabPanel header='Code'>{code}</TabPanel>
+      </TabView>
+    </div>
   )
 }
