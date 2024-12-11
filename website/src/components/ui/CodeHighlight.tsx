f4444f812d651d0d943af8ff17c0dc5867e6858e
migmac99 <migmacthegamer@gmail.com>
2024-12-11 14:35:44 +0000

    Better sizing for code blocks

--- website/src/components/ui/CodeHighlight.tsx
+++ website/src/components/ui/CodeHighlight.tsx
@@ -6,9 +6,12 @@ interface CodeHighlightProps {
   style?: React.CSSProperties
   codeClassName?: string
 }
+
 export default function CodeHighlight({ code, lang, style, codeClassName }: CodeHighlightProps) {
   const codeElement = useRef()
   const languageClassName = `language-${lang || 'jsx'}`
+
+  code = `\n${code.trim()}\n`
 
   useEffect(() => {
     // @ts-expect-error - Prism is a global variable
