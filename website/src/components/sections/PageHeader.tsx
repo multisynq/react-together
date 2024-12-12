f42ed9838bf4b67adc61743ccff18fdf40626004
elijah lee <84287663+aloha-elijah@users.noreply.github.com>
2024-12-12 15:32:08 -0800

    [website] Revised Landing Page links
    -Updated the "Getting Started" link from onClick to a Link component.
    -Changed the "npm..." text from a copy action to an npm project link that opens in a new tab.

--- website/src/components/sections/PageHeader.tsx
+++ website/src/components/sections/PageHeader.tsx
@@ -1,22 +1,7 @@
-import { Button } from 'primereact/button'
-import { useState } from 'react'
-import { useNavigate } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 export function PageHeader() {
-  const navigate = useNavigate()
-  const INSTALLCODE = `npm i react-together`
-
-  const [copySuccess, setCopySuccess] = useState(false)
-
-  const copyToClipboard = () => {
-    navigator.clipboard
-      .writeText(INSTALLCODE)
-      .then(() => {
-        setCopySuccess(true)
-        setTimeout(() => setCopySuccess(false), 2000)
-      })
-      .catch((err) => console.error('Failed to copy text: ', err))
-  }
+  const npmLink = 'https://www.npmjs.com/package/react-together'
 
   return (
     <div className='flex flex-col items-center max-w-[105rem]'>
@@ -32,25 +17,18 @@ export function PageHeader() {
       </div>
       <span className='h-8' />
       <div className='flex gap-2 flex-col sm:flex-row'>
-        <div
-          className='px-4 py-2 bg-blue-300 text-gray-800 text-center flex items-center justify-center cursor-pointer line-border hover:shadow-lineStyleMedium hover:bg-blue-100'
-          onClick={() => navigate('/getting-started')}
-        >
-          <h6 className='font-semibold'>Getting Started</h6>
-        </div>
-        <div className='flex items-center bg-white px-4 line-border gap-1 h-11'>
-          <span>
-            <p className='font-mono'>{INSTALLCODE}</p>
-          </span>
-          <Button
-            icon={copySuccess ? 'pi pi-check' : 'pi pi-copy'}
-            text
-            severity='secondary'
-            aria-label='Bookmark'
-            className='w-7 h-6'
-            onClick={copyToClipboard}
-          />
-        </div>
+        <Link to='/getting-started'>
+          <div className='px-4 py-2 bg-blue-300 flex justify-center line-border line-border-hover hover:bg-blue-100'>
+            <span className='font-semibold text-center'>Getting Started</span>
+          </div>
+        </Link>
+        <a href={npmLink} target='_blank'>
+          <div className='flex items-center bg-white px-4 line-border line-border-hover gap-1 h-11 hover:bg-gray-200'>
+            <span>
+              <p className='font-mono'>npm i react-together</p>
+            </span>
+          </div>
+        </a>
       </div>
       <span className='h-2'></span>
     </div>
