40ab032545aacb8c23d0533f8dd74c973a1cd42a
afonsocrg <afonso.crg@gmail.com>
2025-01-09 21:27:23 +0000

    remove object-hash from package

--- packages/react-together/vite.config.ts
+++ packages/react-together/vite.config.ts
@@ -20,6 +20,7 @@ export default defineConfig({
           'react',
           '@croquet',
           'color-hash',
+          'object-hash',
           'primereact',
           'primeicons',
           'unique-names-generator'
